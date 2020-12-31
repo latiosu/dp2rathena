1bbbbf7e1d8d909d518dbabd1fc9bc8af0b6c826
Eric <latiosworks@gmail.com>
2020-12-31 13:56:54 +1100

    Refactoring and clean up line length

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,15 @@
-from context import dp2rathena
-from dp2rathena import converter
-
-import pytest
 import filecmp
 import os
+
 import json
+import pytest
+
+from context import dp2rathena
+from dp2rathena import item_mapper
 
 # TODO: implement remaining item mapper functions
 # def test_item_mapper():
-#     convert = converter.Converter()
+#     convert = item_mapper.Mapper()
 #     item_json = open(os.path.dirname(os.path.realpath(__file__)) + '/fixtures/item_1101.json').read()
 #     generated_yml = convert.to_item_yml(json.loads(item_json))
 #     print(generated_yml)
@@ -16,7 +17,7 @@ import json
 #     assert generated_yml == expected_yml
 
 def test_mapping_name():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.name({'name': None}) == ''
     assert mapper.name({'name': ''}) == ''
     assert mapper.name({'name': '100T Zeny Check'}) == '100T Zeny Check'
@@ -26,8 +27,9 @@ def test_mapping_name():
     assert mapper.name({'name': 'Travel Brochure [Amatsu]'}) == 'Travel Brochure [Amatsu]'
     assert mapper.name({'name': 'MATK+1%(Lower)'}) == 'MATK+1%(Lower)'
 
+
 def test_mapping_itemTypeId():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.itemTypeId({'itemTypeId': 0, 'itemSubTypeId': 0}) is None
     with pytest.raises(AssertionError):
         mapper.itemTypeId({'itemTypeId': -1, 'itemSubTypeId': -1})
@@ -41,32 +43,38 @@ def test_mapping_itemTypeId():
     assert mapper.itemTypeId({'itemTypeId': 9, 'itemSubTypeId': 0}) == 'Armor'
     assert mapper.itemTypeId({'itemTypeId': 10, 'itemSubTypeId': 0}) == 'ShadowGear'
 
+
 def test_mapping_itemSubTypeId():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.itemSubTypeId({'itemTypeId': 0, 'itemSubTypeId': 0}) is None
     with pytest.raises(AssertionError):
         mapper.itemSubTypeId({'itemTypeId': -1, 'itemSubTypeId': -1})
         mapper.itemSubTypeId({'itemTypeId': 1, 'itemSubTypeId': -1})
-    assert mapper.itemSubTypeId({'itemTypeId': 4, 'itemSubTypeId': 1025}) == 'Arrow/Dagger/Bullet/Shell/Grenade/Shuriken/Kunai/CannonBall/ThrowWeapon'
-    assert mapper.itemSubTypeId({'itemTypeId': 4, 'itemSubTypeId': 0}) == 'Arrow/Dagger/Bullet/Shell/Grenade/Shuriken/Kunai/CannonBall/ThrowWeapon'
+    assert mapper.itemSubTypeId({'itemTypeId': 4, 'itemSubTypeId': 1025}) \
+        == 'Arrow/Dagger/Bullet/Shell/Grenade/Shuriken/Kunai/CannonBall/ThrowWeapon'
+    assert mapper.itemSubTypeId({'itemTypeId': 4, 'itemSubTypeId': 0}) \
+        == 'Arrow/Dagger/Bullet/Shell/Grenade/Shuriken/Kunai/CannonBall/ThrowWeapon'
     assert mapper.itemSubTypeId({'itemTypeId': 1, 'itemSubTypeId': 258}) == '2hSword'
     assert mapper.itemSubTypeId({'itemTypeId': 1, 'itemSubTypeId': 0}) is None
     assert mapper.itemSubTypeId({'itemTypeId': 2, 'itemSubTypeId': 512}) is None
     assert mapper.itemSubTypeId({'itemTypeId': 10, 'itemSubTypeId': 526}) is None
 
+
 def test_mapping_sell():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.sell({}) is None
 
+
 def test_mapping_weight():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.weight({'weight': 0}) == 0
     assert mapper.weight({'weight': 50.0}) == 500
     assert mapper.weight({'weight': 0.1}) == 1
     assert mapper.weight({'weight': -1}) == 0
 
+
 def test_mapping_job():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.job({'job': None}) is None
     assert mapper.job({'job': 0xFFFFF}) is None
     assert mapper.job({'job': 0}) is None
@@ -78,9 +86,10 @@ def test_mapping_job():
     assert mapper.job({'job': 142}) == {'Summoner': True}
     assert mapper.job({'job': 144}) == {'KagerouOboro': True, 'Rebellion': True}
 
+
 # TODO ...
 # def test_mapping_classNum():
-#     mapper = converter.Mapper()
+#     mapper = item_mapper.Mapper()
 #     assert mapper.classNum({'classNum': 0}) is None
 #     assert mapper.classNum({'classNum': -1}) is None
 #     assert mapper.classNum({'classNum': 1}) == ''
@@ -88,7 +97,7 @@ def test_mapping_job():
 #     assert mapper.classNum({'classNum': 10}) == ''
 
 def test_mapping_gender():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.gender({'job': None}) is None
     assert mapper.gender({'job': 1}) is None
     assert mapper.gender({'job': 0xFFFFF}) is None
@@ -100,7 +109,7 @@ def test_mapping_gender():
     assert mapper.gender({'job': 0x18000}) == 'Both'
 
 def test_mapping_locationId():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.locationId({'locationId': None, 'itemTypeId': 0}) is None
     assert mapper.locationId({'locationId': 0, 'itemTypeId': 0}) is None
     with pytest.raises(AssertionError):
@@ -108,15 +117,17 @@ def test_mapping_locationId():
         mapper.locationId({'locationId': 0x400000, 'itemTypeId': 0})
     assert mapper.locationId({'locationId': 0x10, 'itemTypeId': 0}) == {'Armor': True}
     assert mapper.locationId({'locationId': 0, 'itemTypeId': 4}) == {'Ammo': True}
-    assert mapper.locationId({'locationId': 0x10000, 'itemTypeId': 0}) == {'Shadow_Armor': True}
-    assert mapper.locationId({'locationId': 0x400, 'itemTypeId': 0}) == {'Costume_Head_Top': True}
     assert mapper.locationId({'locationId': 0x22, 'itemTypeId': 0}) == {'Both_Hand': True}
     assert mapper.locationId({'locationId': 0x88, 'itemTypeId': 0}) == {'Both_Accessory': True}
-    assert mapper.locationId({'locationId': 0x300, 'itemTypeId': 0}) == {'Head_Top': True, 'Head_Mid': True}
-    assert mapper.locationId({'locationId': 0x301, 'itemTypeId': 0}) == {'Head_Top': True, 'Head_Mid': True, 'Head_Low': True}
+    assert mapper.locationId({'locationId': 0x10000, 'itemTypeId': 0}) == {'Shadow_Armor': True}
+    assert mapper.locationId({'locationId': 0x400, 'itemTypeId': 0}) == {'Costume_Head_Top': True}
+    assert mapper.locationId({'locationId': 0x300, 'itemTypeId': 0}) \
+        == {'Head_Top': True, 'Head_Mid': True}
+    assert mapper.locationId({'locationId': 0x301, 'itemTypeId': 0}) \
+        == {'Head_Top': True, 'Head_Mid': True, 'Head_Low': True}
 
 def test_mapping_itemLevel():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.itemLevel({'itemTypeId': 0}) is None
     with pytest.raises(AssertionError):
         mapper.itemLevel({'itemTypeId': -1})
@@ -124,7 +135,7 @@ def test_mapping_itemLevel():
     assert mapper.itemLevel({'itemTypeId': 2}) is None
 
 def test_mapping_itemMoveInfo():
-    mapper = converter.Mapper()
+    mapper = item_mapper.Mapper()
     assert mapper.itemMoveInfo({'itemMoveInfo': {
         'drop': True,
         'trade': True,
